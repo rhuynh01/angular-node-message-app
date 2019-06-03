d4651aeab644d0edb1f433451c0f98235f48d7cb
Rodney Huynh <rhuynh2005@gmail.com>
2019-06-03 17:22:52 -0500

    first commit

---
+++ backend/app.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
+
+const app = express();
+
+// Connect to localhost mongoDB:
+// mongoose.connect('mongodb://localhost:27017/node-angular', {useNewUrlParser: true})
+mongoose
+  .connect(
+    "mongodb+srv://rodney_dev:" +
+    process.env.MONGO_ATLAS_PW +
+    "@cluster0-rxkkj.mongodb.net/node-angular"
+  )  
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
+
+module.exports = app;
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: "http://nodeangular-env.92z33qqcmd.us-east-2.elasticbeanstalk.com/api"
+};
